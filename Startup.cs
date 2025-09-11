1b9a506e173c5a7e0cac323ae46a2e52f8232b1e
kazi-kamruddin <kazikamruddinahmed@gmail.com>
2025-09-11 12:23:11 +0600

    register, login, logout, passReset

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SkillBridge.Startup))]
+namespace SkillBridge
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ skillBridge queries.sql
@@ -0,0 +1,6 @@
+use SkillBridgeDb;
+
+select * from dbo.AspNetUsers;
+
+DELETE FROM dbo.AspNetUsers
+WHERE Email = 'kazi@gmail.com';
