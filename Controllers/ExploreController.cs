5ad3ee38587430ba8f766c1e913c480eebae320f
kazi-kamruddin <kazikamruddinahmed@gmail.com>
2025-09-12 17:49:11 +0600

    explore page and user profile page with minimal info(#6)

---
+++ Controllers/ExploreController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNet.Identity;
+using SkillBridge.Models;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+
+public class ExploreController : Controller
+{
+    private ApplicationDbContext db = new ApplicationDbContext();
+
+    public ActionResult Index()
+    {
+        var currentUserId = User.Identity.GetUserId();
+
+        // TODO: Later: implement best match / partial match logic
+        var model = new ExploreViewModel
+        {
+            BestMatches = new List<PublicProfileViewModel>(), // empty for now
+            PartialMatches = new List<PublicProfileViewModel>()
+        };
+
+        return View(model);
+    }
+}
---
+++ Models/ExploreViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace SkillBridge.Models
+{
+    public class ExploreViewModel
+    {
+        public List<PublicProfileViewModel> BestMatches { get; set; }
+        public List<PublicProfileViewModel> PartialMatches { get; set; }
+    }
+}
